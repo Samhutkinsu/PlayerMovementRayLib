ef2d5a81b4795e7958717968fa1462dcbd129349
Yasin <25YasinS@mydacc.org>
2023-11-29 10:03:32 -0500

    Initial Commit 11/29/23

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ namespace ShapesRay
             int recY = 0;
             int screenWidth = 800;
             int screenHeight = 400;
+            
             Raylib.InitWindow(screenWidth, screenHeight, "ShapesRay"); // Intilizes window
 
 
             Image image = Raylib.LoadImage("C:\\Users\\25YasinS\\Documents\\Other\\ShapesRay\\assets\\PlayerIcon.png"); //Loads Image from Computer
             Texture2D playerTexture = Raylib.LoadTextureFromImage(image); //Turns image into texture
-            Raylib.UnloadImage(image); //Unloads image from texture
+            Raylib.UnloadImage(image); //Unloads image 
 
             recX = screenWidth / 2 - playerTexture.Width/2; // Player's Starting X
             recY = screenHeight / 2 - playerTexture.Height/2; //Player's Starting Y
@@ -32,13 +33,14 @@ namespace ShapesRay
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) { recY -= 3; }
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) { recY += 3; }
                     Raylib.ClearBackground(Color.RAYWHITE); // Draws Background
-                    Raylib.DrawRectangle(recX, recY, 30, 30, Color.BLUE); // Draws rectangle in place of sprite
+                    Raylib.DrawRectangle(recX , recY, 30, 30, Color.BLUE); // Draws rectangle in place of sprite
                     Raylib.DrawTexture(playerTexture, recX, recY, Color.WHITE); // Draws Players Sprite
                     Raylib.DrawText("RPG Movement Program", 285, 12, 20, Color.LIGHTGRAY); // Draws text on top of the screen
                     Raylib.DrawText($"Players X Position: {recX}, Players Y Position: {recY}", screenWidth/5, 350, 20, Color.LIGHTGRAY); // Debug
+                    Raylib.DrawFPS(screenWidth/2-20,30);
                 Raylib.EndDrawing(); // ends drawing
             }
-            Raylib.UnloadTexture(playerTexture);
+            Raylib.UnloadTexture(playerTexture); // UNloads Texture
             Raylib.CloseWindow(); // Closes Window
         }
     }
